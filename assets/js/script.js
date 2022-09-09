f90f4495f3433d9f7f6cc34f37a1235581d6192e
Kaidan <kaidangustave@yahoo.com>
2022-09-09 17:03:59 -0500

    reorganized and restructed code with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,59 @@
-let now = moment();
-__debugTimeSetToTestMode(); // TODO remove later
-let scheduleTable = $('#schedule-table');
+let now = moment(); // get the current time
+let scheduleTable = $('#schedule-table'); // grab the schedule table to populate with rows
 
-for(hour in data) {
-    let time = moment();
-    time.set({'hour': parseInt(hour), 'minute': 0, 'second': 0, 'millisecond': 0});
-    createScheduleRow(time, data[hour]);
-}
+// this function will help generate columns in a less messy way
+let generateScheduleColumn = (type, time) => $('<div>').attr('id', `col-${type}-${time.format('h-a')}`);
 
+// function for generating a full row representing the schedule for a given hour.
 function createScheduleRow(time, event) {
-    console.log(event);
-
-    let timeColumn = $('<div>')
-            .attr('id', `col-time-${time.format('h-a')}`)
-            .addClass(['align-middle', 'col-auto', 'w-10', 'border', 'border-left-0'])
-            .text(time.format('h a'));
-
-    let eventClasses = ['col', 'd-flex', 'pl-0', 'pr-0','text-light'];
-    if(time.hour() === now.hour()) {
-        eventClasses.push('bg-danger');
-    } else if(time.isBefore(now)) {
-        eventClasses.push('bg-secondary');
-    } else {
-        eventClasses.push('bg-success');
+    // setup event column classes for the given row
+    let eventClasses = ['col', 'd-flex', 'pl-0', 'pr-0', 'text-light'];
+    // depending on what time it is, this will either be gray (past), red (present), or green (future)
+    eventClasses.push(time.hour() === now.hour() ? 'bg-danger' : time.isBefore(now) ? 'bg-secondary' : 'bg-success');
+    // create the text area for each event column
+    let eventTextArea = $('<textarea>').addClass(['flex-fill', 'border-0']);
+    // if there is previously saved content, we need to grab that and put it into the text area when the page loads
+    if(event) {
+        console.log(event); // TODO remove this
+        eventTextArea.text(event); // set text to event
     }
-
-    let eventColumn = $('<div>')
-            .attr('id', `col-event-${time.format('h-a')}`)
-            .addClass(eventClasses)
-            .append($('<textarea>').addClass(['flex-fill', 'border-0']));
+    // create save button for save column
+    let saveButton = $('<button>')
+        .addClass(['btn', 'flex-fill'])
+        .append($('<i>').addClass(['fas', 'fa-save']));
+    // create time column
+    let timeColumn = generateScheduleColumn('time', time)
+        .addClass(['align-middle', 'col-auto', 'w-10', 'border', 'border-left-0'])
+        .text(time.format('h a'));
+    // create event column
+    let eventColumn = generateScheduleColumn('event', time)
+        .addClass(eventClasses)
+        .append(eventTextArea);
     // TODO Add input function
-
-    let saveColumn = $('<div>')
-            .attr('id', `col-save-${time.format('h-a')}`)
-            .addClass(['col-auto', 'w-10', 'text-light', 'border', 'border-right-0']);
-
+    // create save column
+    let saveColumn = generateScheduleColumn('save', time)
+        .addClass(['col-auto', 'd-flex', 'align-items-center', 'w-10', 'text-light', 'border', 'border-right-0'])
+        .append(saveButton);
+    // create the schedule row, appending each column in the proper order
     let scheduleRow = $('<div>').addClass('row')
         .append(timeColumn)
         .append(eventColumn)
         .append(saveColumn);
-
+    // append the row to the table
     scheduleTable.append(scheduleRow);
 }
 
-function __debugTimeSetToTestMode() {
-    now.set({'hour': 12, 'minute': 0, 'second': 0, 'millisecond': 0});
+// this function is available via a gitignored script
+// it will only load if the script is loaded via HTML, which for the deployed application the script tag to load it is commented out
+if((typeof __debugTimeSetToTestMode) !== 'undefined') {
+    __debugTimeSetToTestMode();
+}
+
+// iterate across each hour in an average 9-5 and create a row
+for(hour in data) {
+    // create a new moment and use that to manage the individual row in relation to the current time
+    let time = moment();
+    time.set({'hour': parseInt(hour), 'minute': 0, 'second': 0, 'millisecond': 0});
+    // create the row
+    createScheduleRow(time, data[hour]);
 }
