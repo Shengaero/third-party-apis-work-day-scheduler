eccb3825015f46bea334a3c4a461498f5f84b86b
Kaidan <kaidangustave@yahoo.com>
2022-09-09 17:52:47 -0500

    more reformatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,45 +7,44 @@ if((typeof now) === 'undefined') {
 let scheduleTable = $('#schedule-table'); // grab the schedule table to populate with rows
 
 // this function will help generate columns in a less messy way
-let generateScheduleColumn = (type, time) => $('<div>').attr('id', `col-${type}-${time.format('h-a')}`);
+let generateScheduleColumn = (el, type, time) => el.attr('id', `col-${type}-${time.format('h-a')}`);
 
 // function for generating a full row representing the schedule for a given hour.
 function createScheduleRow(time, event) {
     // setup event column classes for the given row
-    let eventClasses = ['col', 'd-flex', 'pl-0', 'pr-0', 'text-light'];
+    let eventClasses = ['col', 'd-flex', 'pl-2', 'pr-2', 'border-0', 'text-light'];
     // depending on what time it is, this will either be gray (past), red (present), or green (future)
     eventClasses.push(time.hour() === now.hour() ? 'bg-danger' : time.isBefore(now) ? 'bg-secondary' : 'bg-success');
-    // create the text area for each event column
-    let eventTextArea = $('<textarea>').addClass(['flex-fill', 'border-0']);
-    // if there is previously saved content, we need to grab that and put it into the text area when the page loads
-    if(event) {
-        console.log(event); // TODO remove this
-        eventTextArea.text(event); // set text to event
-    }
-    // create save button for save column
-    let saveButton = $('<button>')
-        .addClass(['btn', 'flex-fill'])
-        .append($('<i>').addClass(['fas', 'fa-save']));
-    // create time column
-    let timeColumn = generateScheduleColumn('time', time)
-        .addClass(['align-middle', 'col-auto', 'w-10', 'border', 'border-left-0'])
-        .text(time.format('h a'));
-    // create event column
-    let eventColumn = generateScheduleColumn('event', time)
-        .addClass(eventClasses)
-        .append(eventTextArea);
-    // TODO Add input function
-    // create save column
-    let saveColumn = generateScheduleColumn('save', time)
-        .addClass(['col-auto', 'd-flex', 'align-items-center', 'w-10', 'text-light', 'border', 'border-right-0'])
-        .append(saveButton);
+
+    let timeColumn = generateScheduleColumn($('<div>'), 'time', time)               // create time column
+        .text(time.format('h a'))                                                   // set text to show the hour represented by the row
+        .addClass(['align-middle', 'col-auto', 'w-10', 'border', 'border-left-0'])  // add classes
+
+    let eventColumn = generateScheduleColumn($('<textarea>'), 'event', time);   // create event column
+    if(event) eventColumn.text(event);                                          // if there is previously saved content, we need to put it into the text area
+    eventColumn.addClass(eventClasses);                                         // add classes
+
+    let saveButton = generateScheduleColumn($('<button>'), 'save', time)                // create save column
+        .append($('<i>').addClass(['fas', 'fa-save']))                                  // append floppy disk icon
+        .on('click', clickSaveButton)                                                   // add on-click listener
+        .addClass([                                                                     // add classes
+            'btn', 'shadow-none', 'col-auto', 'd-flex', 'align-items-center',
+            'w-10', 'text-light', 'border-right-0', 'rounded-0'
+        ]);
+
     // create the schedule row, appending each column in the proper order
     let scheduleRow = $('<div>').addClass('row')
         .append(timeColumn)
         .append(eventColumn)
-        .append(saveColumn);
+        .append(saveButton);
+
     // append the row to the table
     scheduleTable.append(scheduleRow);
+}
+
+// Will run when the save button is clicked
+function clickSaveButton() {
+    // TODO Implement
 }
 
 // iterate across each hour in an average 9-5 and create a row
