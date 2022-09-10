c3fd71acb4a42dacee426566fb239adb1ff1f88e
Kaidan <kaidangustave@yahoo.com>
2022-09-09 20:02:26 -0500

    touched up certain text alignment and justification

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,21 +18,24 @@ function createScheduleRow(time, event) {
     // depending on what time it is, this will either be gray (past), red (present), or green (future)
     eventClasses.push(time.hour() === now.hour() ? 'bg-danger' : time.isBefore(now) ? 'bg-secondary' : 'bg-success');
 
-    let timeColumn = generateScheduleColumn($('<div>'), 'time', time)               // create time column
-        .text(time.format('h a'))                                                   // set text to show the hour represented by the row
-        .addClass(['align-middle', 'col-auto', 'w-10', 'border', 'border-left-0'])  // add classes
+    let timeColumn = generateScheduleColumn($('<div>'), 'time', time)   // create time column
+        .append($('<p>').text(time.format('h a')))                      // set text to show the hour represented by the row
+        .addClass([                                                     // add classes
+            'd-flex', 'align-items-center', 'justify-content-center',
+            'col-auto', 'w-10', 'border', 'border-left-0'
+        ]);
 
     let eventColumn = generateScheduleColumn($('<textarea>'), 'event', time)    // create event column
         .on('change', () => eventColumn.val(eventColumn.val().trim()));         // listen for changes to text so that we can trim unnecessary whitespace
     if(event) eventColumn.text(event);                                          // if there is previously saved content, we need to put it into the text area
     eventColumn.addClass(eventClasses);                                         // add classes
 
-    let saveButton = generateScheduleColumn($('<button>'), 'save', time)                // create save column
-        .append($('<i>').addClass(['fas', 'fa-save']))                                  // append floppy disk icon
-        .on('click', clickSaveButton)                                                   // add on-click listener
-        .addClass([                                                                     // add classes
-            'btn', 'shadow-none', 'col-auto', 'd-flex', 'align-items-center',
-            'w-10', 'text-light', 'border-right-0', 'rounded-0'
+    let saveButton = generateScheduleColumn($('<button>'), 'save', time)    // create save column
+        .append($('<i>').addClass(['fas', 'fa-save']))                      // append floppy disk icon
+        .on('click', clickSaveButton)                                       // add on-click listener
+        .addClass([                                                         // add classes
+            'btn', 'shadow-none', 'col-auto', 'd-flex', 'justify-content-center',
+            'align-items-center', 'w-10', 'text-light', 'border-right-0', 'rounded-0'
         ]);
 
     // create the schedule row, appending each column in the proper order
@@ -83,4 +86,3 @@ for(hour in data) {
     // create the row
     createScheduleRow(time, data[hour]);
 }
-// scheduleTable.append($('<button>').addClass(['btn', 'btn-danger', 'float-right', 'w-10']).append($('<i>').addClass(['fas', 'fa-trash-alt'])))
